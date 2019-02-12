66bc0608b954e4b05f2851a212cc106f0253321a
Alex Harness <aharness@hashicorp.com>
2019-02-12 09:46:47 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,15 +40,15 @@ module "ec2_cluster" {
   source  = "app.terraform.io/aharness-org/ec2-instance/aws"
   version = "1.14.0"
 
-  name                   = "my-cluster"
+  name                   = "my-consumer-cluster"
   instance_count         = 2
 
   ami                    = "${data.aws_ami.ubuntu.id}"
   instance_type          = "t2.micro"
   monitoring             = true
 
-  vpc_security_group_ids = ["${module.vpc.default_security_group_id}"]
-  subnet_id              = "${element(module.vpc.public_subnets, 0)}"
+  vpc_security_group_ids = ["${module.vpc.producer.default_security_group_id}"]
+  subnet_id              = "${element(module.vpc.producer.public_subnets, 0)}"
 
   tags = {
     Terraform = "true"
