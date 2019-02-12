3170a710bf2f00353734d91d8d917565520902cf
Alex Harness <aharness@hashicorp.com>
2019-02-12 14:01:33 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,3 +35,22 @@ resource "aws_instance" "web" {
     Name = "Research Instance"
   }
 }
+module "ec2_cluster" {
+  source  = "app.terraform.io/aharness-org/ec2-instance/aws"
+  version = "1.15.0"
+
+  name                   = "my-consumer-cluster"
+  instance_count         = 2
+
+  ami                    = "${data.aws_ami.ubuntu.id}"
+  instance_type          = "t2.micro"
+  monitoring             = true
+
+  vpc_security_group_ids = ["${data.terraform_remote_state.network.default_security_group_id}"]
+  subnet_id              = "${data.terraform_remote_state.network.research_subnet_id}"
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
