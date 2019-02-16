f22321eb6bf0c7ecf084d5fc1cdfedf001489ca1
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-02-15 23:13:32 -0600

    Update consumer-frontend-app.tf

--- consumer-frontend-app.tf
+++ consumer-frontend-app.tf
@@ -5,7 +5,7 @@ variable "number_of_instances" {
 
 variable "name" {
   description = "The name of the app deployed"
-  default = "Consumer-FrontEnd"
+  default = "Consumer-App"
 }
 
 module "elb" {
