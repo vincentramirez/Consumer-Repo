8cde7c5a40ff6300b4e2066a80b4fe85c750a77d
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-02-15 20:54:37 -0600

    Update loadbalancerweb.tf

--- loadbalancerweb.tf
+++ loadbalancerweb.tf
@@ -3,6 +3,22 @@ variable "number_of_instances" {
   default     = 2
 }
 
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 module "elb" {
   source = "app.terraform.io/aharness-org/elb/aws"
   version = "1.4.1"
