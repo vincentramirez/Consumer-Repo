bf0ad83df266d32162adde2078137a461753c223
Vinnie Ramirez <vinnie.ramirez@gmail.com>
2019-02-22 21:47:36 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
   subnet_id = "${data.terraform_remote_state.network.production_subnet_id}"
 
   tags {
